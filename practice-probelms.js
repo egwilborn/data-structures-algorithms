91054ffa94a6eef493ac0645e1b1eb1713323320
egwilborn <egwilborn@gmail.com>
2023-10-03 12:15:10 -0500

    adds leet code problem

--- practice-probelms.js
+++ practice-probelms.js
@@ -137,3 +137,45 @@ function isSubsequence(subString, string) {}
 // Time Complexity - O(N + M)
 
 // Space Complexity - O(1)
+
+//LEET CODE PROBLEMS /////////////////
+// 1) Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+function twoSum(arr, target) {
+  let map = {};
+  arr.forEach(function (element, idx) {
+    map[idx] = element;
+  });
+
+  for (i = 0; i < arr.length; i++) {
+    let diff = target - arr[i];
+    if (Object.values(map).includes(diff)) {
+      return [i, arr.indexOf(diff)];
+    }
+  }
+}
+console.log(twoSum([1, 8, 7, 20, 6, 10], 18));
