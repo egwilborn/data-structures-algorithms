850e7978b2410032c6fd7b35dcc98e76902d077a
egwilborn <egwilborn@gmail.com>
2023-10-04 16:59:33 -0500

    adds bubble sort with output

--- bubble-sort.js
+++ bubble-sort.js
@@ -7,8 +7,10 @@
 function bubbleSort(arr) {
   let noSwaps;
   for (let i = 0; i < arr.length; i++) {
+    // console.log("i-loop:", i, "array:", arr);
     noSwaps = true;
     for (let j = 0; j < arr.length; j++) {
+      //   console.log("j-loop:", j, "arr:", arr);
       if (arr[j] > arr[j + 1]) {
         //swap!
         let temp = arr[j];
@@ -21,3 +23,106 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
+console.log(bubbleSort([1, 3, 5, 2, 6, 4, 9]));
+//output ---------
+// i-loop: 0 array: [
+//     1, 3, 5, 2,
+//     6, 4, 9
+//   ]
+//   j-loop: 0 arr: [
+//     1, 3, 5, 2,
+//     6, 4, 9
+//   ]
+//   j-loop: 1 arr: [
+//     1, 3, 5, 2,
+//     6, 4, 9
+//   ]
+//   j-loop: 2 arr: [
+//     1, 3, 5, 2,
+//     6, 4, 9
+//   ]
+//   j-loop: 3 arr: [
+//     1, 3, 2, 5,
+//     6, 4, 9
+//   ]
+//   j-loop: 4 arr: [
+//     1, 3, 2, 5,
+//     6, 4, 9
+//   ]
+//   j-loop: 5 arr: [
+//     1, 3, 2, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 6 arr: [
+//     1, 3, 2, 5,
+//     4, 6, 9
+//   ]
+//   i-loop: 1 array: [
+//     1, 3, 2, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 0 arr: [
+//     1, 3, 2, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 1 arr: [
+//     1, 3, 2, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 2 arr: [
+//     1, 2, 3, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 3 arr: [
+//     1, 2, 3, 5,
+//     4, 6, 9
+//   ]
+//   j-loop: 4 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 5 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 6 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   i-loop: 2 array: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 0 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 1 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 2 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 3 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 4 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 5 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   j-loop: 6 arr: [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
+//   [
+//     1, 2, 3, 4,
+//     5, 6, 9
+//   ]
