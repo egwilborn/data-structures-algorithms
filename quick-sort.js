dbb263024c37aca6396669f274f6fa4bbc412cd6
egwilborn <egwilborn@gmail.com>
2023-10-10 10:26:52 -0500

    finished quick sort

--- quick-sort.js
+++ quick-sort.js
@@ -8,4 +8,40 @@
 // – swap the starting element (i.e. the pivot) with the pivot index
 // – return the pivot index
 
-function pivot(arr, start, end) {}
+function pivot(arr, start = 0, end = arr.length + 1) {
+  function swap(array, i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  let pivot = arr[start];
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, start, swapIndex);
+  return swapIndex;
+}
+
+//pivot([4, 8, 2, 1, 5, 7, 6, 3]); // should return 3 -- the correct index of 4
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); // 3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+// returns
+// [
+//     1, 2, 3, 4,
+//     5, 6, 7, 8
+//   ]
