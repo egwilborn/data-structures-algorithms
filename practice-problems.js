2b14b8f56d9347add4644e6c4f3975f200c9b392
egwilborn <egwilborn@gmail.com>
2024-01-08 19:16:19 -0600

    adds cakes to make function

--- practice-problems.js
+++ practice-problems.js
@@ -469,6 +469,40 @@ function min(arr, toReturn) {
   }
 }
 
-console.log(min([1, 2, 3, 4, 5], "value")); // => 1
-console.log(min([1, 2, 3, 4, 5], "index")); // => 0
-console.log(min([8, 9, 4, 5, 1], "index"));
+// console.log(min([1, 2, 3, 4, 5], "value")); // => 1
+// console.log(min([1, 2, 3, 4, 5], "index")); // => 0
+// console.log(min([8, 9, 4, 5, 1], "index"));
+
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+// Examples:
+
+function cakes(recipe, ingredients) {
+  // define an array to hold the possible number of cakes based on the comparison of each ingredient to recipe
+  let cakeNums = [];
+  for (let key in recipe) {
+    if (key in ingredients) {
+      let cakeNum = Math.floor(ingredients[key] / recipe[key]);
+      cakeNums.push(cakeNum);
+    } else {
+      return 0;
+    }
+  }
+  return Math.min(...cakeNums);
+}
+// must return 2
+// console.log(
+//   cakes(
+//     { flour: 500, sugar: 200, eggs: 1 },
+//     { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+//   )
+// );
+// // must return 0
+// console.log(
+//   cakes(
+//     { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 },
+//     { sugar: 500, flour: 2000, milk: 2000 }
+//   )
+// );
