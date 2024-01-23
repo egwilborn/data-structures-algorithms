77c883e9277eb685fa04a73c245c08e48a60ccb7
egwilborn <egwilborn@gmail.com>
2024-01-22 19:43:32 -0600

    adds majority element challenge problem

--- practice-problems.js
+++ practice-problems.js
@@ -661,3 +661,34 @@ var removeElement = function (nums, val) {
 // console.log(removeElement([3, 2, 2, 3], 3));
 // console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
 // console.log(removeElement([1, 2, 3, 2, 2, 2, 2, 2], 2));
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array.
+
+function majorityElement(nums) {
+  let elementCount = {};
+  nums.forEach((num) => {
+    elementCount[num] ? (elementCount[num] += 1) : (elementCount[num] = 1);
+  });
+  let majorityEl = null;
+  for (const key in elementCount) {
+    if (elementCount[key] > nums.length / 2) {
+      majorityEl = key;
+    }
+  }
+  return majorityEl;
+}
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// majorityElement([3, 2, 3]);
+// majorityElement([2, 2, 1, 1, 1, 2, 2]);
